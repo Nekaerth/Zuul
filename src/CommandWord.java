fbecbea8aadba13962c86cb099568cc5b2097fe2
Nekaerth <lasse.j.lundin@gmail.com>
2016-10-12 12:59:22 +0200

    Opdatered Player, Boss og Game klasserne

--- src/CommandWord.java
+++ src/CommandWord.java
@@ -4,7 +4,7 @@ public enum CommandWord {
 	/**
 	 *
 	 */
-	GO("go"), QUIT("quit"), HELP("help"), SEARCH("search"), UNKNOWN("?"), PICKUP("pickup");
+	GO("go"), QUIT("quit"), HELP("help"), SEARCH("search"), UNKNOWN("?"), PICKUP("pickup"), DROP("drop"), USE("use");
 
 	private String commandString;
 
