b989a64c1586ecf243e4ab9a5d3427992af28e44
Nekaerth <lasse.j.lundin@gmail.com>
2016-10-17 13:08:34 +0200

    Creates bossFight() in Room

--- src/Attack.java
+++ src/Attack.java
@@ -1,10 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
+ * This class represent an attack, which can be used both for the player and the
+ * boss. It contains two Moves attributes; one to represent what kind of attack
+ * it is and one to tell what move can counter the attack. Lastly it contains an
+ * int attribute that tells how much damage the attack does.
  *
  * @author Lasse
  */
@@ -14,26 +13,61 @@ public class Attack {
 	private Moves counterMove = null;
 	private int damage;
 
+	/**
+	 * This constructor returns an Attack-object which is meant for the player,
+	 * because it does not contain a counterMove.
+	 *
+	 * @param move is a enum which represent what kind of attack it is.
+	 * @param damage is how much this attack deals in damage.
+	 */
 	public Attack(Moves move, int damage) {
 		this.move = move;
 		this.damage = damage;
 	}
 
+	/**
+	 * This constructor returns an Attack-object which is meant for the boss,
+	 * because it does contain a counterMove.
+	 *
+	 * @param move is a enum which represent what kind of attack it is.
+	 * @param counterMove is a enum which tells what move can counter this attack.
+	 * @param damage is how much this attack deals in damage.
+	 */
 	public Attack(Moves move, Moves counterMove, int damage) {
 		this.move = move;
 		this.counterMove = counterMove;
 		this.damage = damage;
 	}
 
+	/**
+	 *
+	 * @return The name of the attack, which is also the name of the move.
+	 */
 	public String getName() {
 		return this.move.toString();
 	}
 
+	/**
+	 *
+	 * @return The damage of the attack.
+	 */
 	public int getDamage() {
 		return this.damage;
 	}
 
-	public void setDamage(int damage) {
-		this.damage = damage;
+	/**
+	 *
+	 * @return the move, which is an enum.
+	 */
+	public Moves getMove() {
+		return this.move;
+	}
+
+	/**
+	 *
+	 * @return the counterMove, which is an enum.
+	 */
+	public Moves getCounterMove() {
+		return this.counterMove;
 	}
 }
--- src/Room.java
+++ src/Room.java
@@ -1,3 +1,4 @@
+
 /**
  * The Room class is used to create rooms and methods
  * that can describe the rooms.
@@ -5,6 +6,7 @@
  */
 import java.util.Set;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Room {
 
@@ -14,7 +16,7 @@ public class Room {
 	boolean numberRoom, escapeRoom, lock, hideRoom = false;
 	public Boss boss = null;
 	int number;
-              
+
 	public Room(String description, boolean numberRoom) { //Constructor der tager en string der beskriver rummet
 		this.description = description; //this.desription er variablen i Classen Room.
 		exits = new HashMap<>(); //exits opretter en ny hashmap der indeholder key som string og room som value.
@@ -25,19 +27,23 @@ public class Room {
 			number = 1;
 		}
 	}
-        /**
-         * The setExit method is used to declare which exits a room has
-         * @param direction is a string parameter used to declare where the exit is
-         * @param neighbor is a room parameter used to declare what room the direction refers to 
-         */
+
+	/**
+	 * The setExit method is used to declare which exits a room has
+	 *
+	 * @param direction is a string parameter used to declare where the exit is
+	 * @param neighbor is a room parameter used to declare what room the direction
+	 * refers to
+	 */
 	public void setExit(String direction, Room neighbor) {
 		exits.put(direction, neighbor);
 	}
 
 	public String getShortDescription() {
-		return "You are " +description;
+		return "You are " + description;
 	}
 //Bør fjernes
+
 	public String getLongDescription() {
 		return "You are " + description + "\n" + getExitString(); //Giver en længere beskrivelse af rummet og giver hvilke exits der findes
 	}
@@ -62,79 +68,131 @@ public class Room {
 			return -1;
 		}
 	}
-        
-        /**
-         * The setEscapeRoom method is a setter method 
-         * that sets the value of the boolean escapeRoom to true
-         */
+
+	/**
+	 * The setEscapeRoom method is a setter method that sets the value of the
+	 * boolean escapeRoom to true
+	 */
 	public void setEscapeRoom() {
 		this.escapeRoom = true;
 
 	}
-        /**
-         * The getEscapeRomm is a getter method 
-         * that returns the current value of the boolean escapeRoom
-         * @return will return the current boolean value of escapeRoom
-         */
+
+	/**
+	 * The getEscapeRomm is a getter method that returns the current value of the
+	 * boolean escapeRoom
+	 *
+	 * @return will return the current boolean value of escapeRoom
+	 */
 	public boolean getEscapeRoom() {
 		return this.escapeRoom;
 
-        /**
-         * The lockRoom method is a setter method
-         * that will set the boolean value lock to true
-         */
+		/**
+		 * The lockRoom method is a setter method that will set the boolean value
+		 * lock to true
+		 */
 	}
-	public void LockRoom(){
+
+	public void LockRoom() {
 		lock = true;
 	}
-        
-        /**
-         * The isLocked method will return the current boolean value of lock
-         * @return will return the current boolean value of lock
-         */
-	public boolean isLocked(){		
+
+	/**
+	 * The isLocked method will return the current boolean value of lock
+	 *
+	 * @return will return the current boolean value of lock
+	 */
+	public boolean isLocked() {
 		return lock;
 	}
-        
-        /**
-         * The unlock method is a setter method 
-         * that will set the boolean value of lock to false
-         */
-	public void unlock(){
+
+	/**
+	 * The unlock method is a setter method that will set the boolean value of
+	 * lock to false
+	 */
+	public void unlock() {
 		lock = false;
 	}
 
-        /**
-         * The isNumberRoom method will return the current boolean value of numberRoom
-         * @return will return the current boolean value 
-         */
+	/**
+	 * The isNumberRoom method will return the current boolean value of numberRoom
+	 *
+	 * @return will return the current boolean value
+	 */
 	public boolean isNumberRoom() {
 		return numberRoom;
 	}
-        
-        /**
-         * The hideRoom method is a setter method
-         * that will set the boolean value hideRoom to true
-         */
-        public void hideRoom() {
-            hideRoom = true;
-        }
-        
-        /**
-         * The showRoom method is a setter method
-         * that will set the boolean value of hideRoom to false
-         */
-        public void showRoom() {
-            hideRoom = false;
-        }           
-        
-        /**
-         * The isHidden method will return the current value of hideRoom
-         * @return will return the current value of the boolean hideRoom
-         */
-        public boolean isHidden() {
-            return hideRoom;
-        }
-        
+
+	/**
+	 * The hideRoom method is a setter method that will set the boolean value
+	 * hideRoom to true
+	 */
+	public void hideRoom() {
+		hideRoom = true;
+	}
+
+	/**
+	 * The showRoom method is a setter method that will set the boolean value of
+	 * hideRoom to false
+	 */
+	public void showRoom() {
+		hideRoom = false;
+	}
+
+	/**
+	 * The isHidden method will return the current value of hideRoom
+	 *
+	 * @return will return the current value of the boolean hideRoom
+	 */
+	public boolean isHidden() {
+		return hideRoom;
+	}
+
+	/**
+	 *
+	 * @param player
+	 * @return
+	 */
+	public boolean bossFight(Player player) {
+		//This loop runs until the player or the boss has no hipoints left. In each iteration the boss attacks once and the player defence once.
+		while (player.getHitpoint() > 0 && boss.getHitpoint() > 0) {
+			Attack currentBossAttack = boss.getRandomAttack(); //This chooses what attack the boss uses at random.
+			Attack currentPlayerAttack = null; //Creates variable for containing what attack the player uses.
+			System.out.println("The prison guard uses " + currentBossAttack.getName() + "!");
+
+			//Within this loop the player chooses a counter attack. This loop makes sure that the input is valid.
+			while (true) {
+				System.out.println("Choose a move: " + player.getAttackString()); //Prints all the available attacks the player can do.
+				System.out.print(">");
+				Scanner scanner = new Scanner(System.in); //Creates new Scanner object.
+				String input = scanner.nextLine(); //Takes user input.
+				currentPlayerAttack = player.getAttack(input); //Checks if the user input is a valid attack and saves the corresponding attack in currentPlayerAttack.
+
+				//Breaks out of the loop if was a valid attack.
+				if (currentPlayerAttack != null) {
+					break;
+				}
+				System.out.println("Move does not exits. Make sure to write a correct move.");
+			}
+
+			//If the chosen attack was a counter attack to the boss attack, the boss loses hitpoint equal to the player attack damage. Else the player loses hitpoints equal to the boss attack damage.
+			if (currentBossAttack.getCounterMove() == currentPlayerAttack.getMove()) { //Checks if the player attack is a counter attack to the boss attack.
+				boss.subtractHitpoint(currentPlayerAttack.getDamage()); //Subtracts hitpoints from the boss.
+				System.out.println("The prison guard loses " + currentPlayerAttack.getDamage() + " hitpoints. He has " + boss.getHitpoint() + " hitpoints left."); //Prints out how much damage dealt and how much hitpoints the boss have left
+			} else {
+				player.subtractHitpoint(currentBossAttack.getDamage()); //Subtracts hitpoints from the player.
+				System.out.println("You lose " + currentBossAttack.getDamage() + " hitpoints. You have " + player.getHitpoint() + " hitpoints left."); //Prints out how much damage dealt and how much hitpoints the player has left.
+			}
+		}
+
+		if (player.getHitpoint() <= 0) {
+			System.out.println("You've been defeated by the prison guard!");
+			return true;
+		} else {
+			System.out.println("You defeated the prison guard!");
+			boss = null;
+			return false;
+		}
+	}
 
 }
