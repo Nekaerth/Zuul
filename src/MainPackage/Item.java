f081c04871b2a11783f076380f7b8c9df2bfea29
Danieln Johansen <Danieln Johansen@DanielSkolepc>
2016-11-21 08:45:09 +0100

    Item er ændret til abstrakt super klasse

--- src/MainPackage/Item.java
+++ src/MainPackage/Item.java
@@ -6,18 +6,40 @@ package MainPackage;
  *
  * @author Semesterprojektgruppe 13 (Autumn 2016)
  */
-public interface Item {
+public abstract class Item {
+	
+	private boolean pickup,useable;
+	private String name;
+	private int weight,capacity;
 
-	public ItemType getType();
+	public Item(boolean pickup, String name, boolean useable, int weight, int capacity){
+		this.pickup = pickup;
+		this.name = name;
+		this.weight = weight;
+		this.capacity = capacity;
+		this.useable = useable;
+	}
+	
+	public abstract ItemType getType();
 
-	public String getName();
+	public String getName(){
+		return this.name;
+	}
 
-	public boolean isPickup();
+	public boolean isPickup(){
+		return this.pickup;
+	}
 
-	public boolean isUseable();
+	public boolean isUseable(){
+		return this.useable;
+	}
 
-	public int getWeight();
+	public int getWeight(){
+		return this.weight;
+	}
 
-	public int getCapacity();
+	public int getCapacity(){
+		return this.capacity;
+	}
 
 }
