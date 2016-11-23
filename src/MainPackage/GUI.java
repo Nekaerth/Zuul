bd7c2353a6d385219086648fb30e347a862ffd34
Nekaerth <lasse.j.lundin@gmail.com>
2016-11-23 13:56:37 +0100

    GUI more changes

--- src/MainPackage/GUI.java
+++ src/MainPackage/GUI.java
@@ -2,13 +2,9 @@ package MainPackage;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
